e148c0fd50dcd573aef5a52d8fbf0df0e5febd02
Vo Hong Phat <vohongphat2892005@gmail.com>
2026-03-02 22:30:16 +0700

    update user backend

---
+++ backend/src/libs/db.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+export const connectDB = async()=>{
+    try{
+        await mongoose.connect(process.env.MONGODB_CONNECTIONSTRING);
+        console.log('Kết nối DB thành công');
+    }catch(error){
+        console.log('Kết nối DB thất bại', error);
+        process.exit(1);
+    }
+
+}
