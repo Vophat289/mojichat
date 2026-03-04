14d96c63798a8aa43ac63057091438c195fb2664
Vo Hong Phat <vohongphat2892005@gmail.com>
2026-03-04 23:20:57 +0700

    update backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import { connectDB } from "./libs/db.js";
 import authRoute from "./routes/authRoute.js"
 import userRoute from "./routes/userRoute.js"
 import cookieParser from "cookie-parser";
+import { protectedRoute } from "./middlewares/authMiddleware.js";
 
 dotenv.config();
 
@@ -18,6 +19,7 @@ app.use(cookieParser());
 app.use('/api/auth', authRoute)
 
 //private routes
+app.use(protectedRoute)
 app.use('/api/users', userRoute)
 
 
